e045c6fe7dc26136f444be3c240c34825a531351
vaibhav-rm <rathodvaibhav401@gmail.com>
2024-08-10 23:13:08 +0530

    created gameloop

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -19,14 +19,21 @@ public class GamePanel extends JPanel implements Runnable{
 	final int ScreenWidth = tileSize * maxScreenCol;
 	final int ScreenHeight = tileSize * maxScreenRow;
 	
+	KeyHandler keyH = new KeyHandler();
 	Thread gameThread;
 	
+	// Set player's default position
+	int playerX = 100;
+	int playerY = 100;
+	int playerSpeed = 4;
 	
 	public GamePanel() {
 		
 		this.setPreferredSize(new Dimension(ScreenWidth, ScreenHeight));
 		this.setBackground(Color.black);
 		this.setDoubleBuffered(true);
+		this.addKeyListener(keyH);
+		this.setFocusable(true);
 		
 	}
 
@@ -55,6 +62,19 @@ public class GamePanel extends JPanel implements Runnable{
 	
 	public void update() {
 		
+		if (keyH.upPressed == true) {
+			playerY -= playerSpeed;
+		}
+		else if (keyH.downPressed == true) {
+			playerY += playerSpeed;
+		}
+		else if (keyH.leftPressed == true) {
+			playerX -= playerSpeed;
+		}
+		else if (keyH.rightPressed == true) {
+			playerX += playerSpeed;
+		}
+		
 	}
 	public void paintComponent(Graphics g) {
 		
@@ -64,7 +84,7 @@ public class GamePanel extends JPanel implements Runnable{
 		
 		g2.setColor(Color.white);
 		
-		g2.fillRect(100, 100, tileSize, tileSize);
+		g2.fillRect(playerX, playerY, tileSize, tileSize);
 		
 		g2.dispose();
 	}
